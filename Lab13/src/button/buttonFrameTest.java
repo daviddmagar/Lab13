95fe289ac4e3eaa7384ed4a30add726034cc3363
David <David@10.30.13.200>
2017-11-28 15:22:16 -0600

    Formatting fixes

--- Lab13/src/button/buttonFrameTest.java
+++ Lab13/src/button/buttonFrameTest.java
@@ -7,16 +7,14 @@ import javax.swing.JFrame;
 public class buttonFrameTest {
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable()
-		{
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				ButtonFrame frame = new ButtonFrame();
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
 				frame.setTitle("Color Generator");
 			}
-		}
-);
+		});
 
 	}
 
